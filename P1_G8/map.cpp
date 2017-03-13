a9e302004ea961efbd34d60c62e49308ada05c53
Bernat <bernatriupuyal@enti.cat>
2017-03-13 20:54:42 +0100

    treball pur i dur

--- P1_G8/map.cpp
+++ P1_G8/map.cpp
@@ -1,17 +1,75 @@
 #include "map.h"
 #include "Input.h"
+#include <iostream>
+#include <conio.h>
 
 
-Map::Map(const int difficulty)
+Map::Map(const int &difficulty)
 {
-	
-	map = new char[difficulty * 5][difficulty*10];
+	mapsize = difficulty * mapproportion;
+	map = new char*[mapsize];
 
+	for (int c = 0; c < mapsize;c++) {
+		map[c] = new char[mapsize];
+	}
 
 }
-void Map::cellModify(int line, int row, char newelement) {
+void Map::cellModify(const int &line, const int &row,const char &newelement) {
 
 	map[line][row] = newelement;
 
 
 }
+
+void operator<<(Map map){
+
+}
+
+Player::Player(Map mapa,int spawnrange) {
+	do {
+		x = (mapa.mapsize / 2) + rand() % mapa.mapsize / spawnrange - mapa.mapsize / 2 * spawnrange;
+		y = (mapa.mapsize/ 2) + rand() % mapa.mapsize/spawnrange - mapa.mapsize/ 2*spawnrange;
+	} while ( mapa.map[x][y] != '$');
+	mapa.cellModify(x, y, '@');
+
+}
+void Player::movement(Map mapa) {
+	char entrada;
+	if (_kbhit()) {
+		entrada = _getch();
+		mapa.cellModify(x,y,'.');
+		switch (entrada) {
+
+		case 'a':
+		case 'A':
+			x--;	break;
+		case 'd':
+		case 'D':
+			x++;	break;
+		case 'w':
+		case 'W':
+			y++;	break;
+		case 's':
+		case 'S':
+			y--;	break;
+		}
+		mapa.cellModify(x, y, '@');
+		}
+	
+}
+
+Coinmanager::Coinmanager(int coinproportion,Map mapa) {
+
+	coinnum = mapa.mapsize*mapa.mapsize / coinproportion;
+
+	for (int c = 0; c < coinnum; c++) {
+		int x;
+		int y;
+		do {
+			x = rand() % mapa.mapsize;
+			y = rand() % mapa.mapsize;
+			
+		} while (mapa.map[x][y] != '.');
+		mapa.map[x][y];
+	}
+ }
--- P1_G8/map.h
+++ P1_G8/map.h
@@ -1,20 +1,49 @@
 #pragma once
+#include "Input.h"
 
 class Map {
 
 public:
 
-	char *map;
+	char **map; // doble punter que apunta la matriu
 
+	const int mapproportion = 5; // ratio de tamany del mapa segons la dificultat [1,5]
 
+	int mapsize; // llargada del mapa.
 
-	Map(const int difficulty);
+	Map(const int &difficulty);
 
-	void cellModify(int line, int row, char newelement);
+	void cellModify(const int &line, const int &row, const char &newelement);
 
 
 	void operator<<(Map map);
 
 
+
+};
+
+class Player {
+
+public:
+
+	int x, y;
+
+	int puntuacio;
+
+	Player(Map mapa,int spawnrange);
+
+	void  movement(Map mapa);
+
+
+};
+
+class Coinmanager {
+
+public:
+	int coinnum;
+
+	const int coinproportion = 10; // proporcio de monedes segons la grandaria del mapa ( 100/3 , 100/13)
+
+	Coinmanager(int coinproportion,Map mapa);
 
 };
